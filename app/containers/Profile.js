04c195d7b8aed3cdd9f87175d4f77cce4810a40f
Kalina Georgieva <kallygeorgieva@gmail.com>
2017-05-07 14:48:15 +0300

    Change Profile container, add components profile-badje, profile-posts, profile-followers, profile-following

--- app/containers/Profile.js
+++ app/containers/Profile.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import ProfileBadge from '../components/profile-info/profle-badge'
+import Posts from '../components/profile-info/profile-posts'
+import Followers from '../components/profile-info/profile-followers'
+import Following from '../components/profile-info/profile-following'
 
-const Profile = () => {
-    return (
-        <div>Profile</div>
-    )
+class Profile extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "Kalina Georgieva",
+            picture: "http://bootdey.com/img/Content/user_1.jpg",
+            followers: [],
+            following: []
+        }
+    }
+
+    render(){
+        return (
+            <div id="profile-page">
+                <Router>
+                    <div>
+                        <ProfileBadge name={this.state.name} picture={this.state.picture} followers={this.state.followers} following={this.state.following}/>
+                        <Route exact path="/profile/posts" component={Posts}/>
+                        <Route path="/profile/followers" component={Followers}/>
+                        <Route path="/profile/following" component={Following}/>
+                    </div>
+                </Router>
+            </div>            
+        )
+    }
 }
 
 export default Profile;
