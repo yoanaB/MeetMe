224fda60cca2bc4a771182946ab1012d358e14e4
Kalina Georgieva <kallygeorgieva@gmail.com>
2017-05-07 03:25:56 +0300

    Create posts component

---
+++ app/components/posts/posts.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Post from './post'
+
+class Posts extends React.Component {
+    
+
+    render() {
+        return <div className="posts">
+                {this.props.posts.map(post =>
+                     <Post key={post.id} author={post.author} userPicture={post.userPicture} msg={post.msg} timestamp={post.timestamp} likes={post.likes} comments={post.comments}/>
+                )};
+            </div>
+    }
+}
+
+export default Posts
