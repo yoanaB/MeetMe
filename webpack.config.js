f46423c2438e27138c6e406ad209905f2db6565c
Kalina Georgieva <kallygeorgieva@gmail.com>
2017-05-07 03:21:48 +0300

    Add webpack.config.js and index.html

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+module.exports = {
+  entry: './app/App.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333,
+    historyApiFallback: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
