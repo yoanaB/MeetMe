c882db3e5b3557a2c985f9f14a8e5f03499b3d17
yoana boyanova <yoana boyanova>
2017-05-23 00:05:24 +0300

    trying to start this

--- homework1Yoana/homework1/src/reducers/index.js
+++ homework1Yoana/homework1/src/reducers/index.js
@@ -1,3 +1,16 @@
 // Set up your root reducer here...
- import { combineReducers } from 'redux';
- export default combineReducers;
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+export const initialState = {
+  posts: [  ]
+};
+
+export function reducer(state = initialState) {
+  return state;
+}
+
+export const combinedReducers = combineReducers({
+  routing: routerReducer,
+  reducer
+});
